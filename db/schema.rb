50e15bb3628b28f63d2f80c8022479d7497fac8d
PatrickStrz <patrick.strzelec@gmail.com>
2017-02-26 00:37:51 -0500

    fixed ideas model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170225234855) do
+ActiveRecord::Schema.define(version: 20170226004000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,11 +18,11 @@ ActiveRecord::Schema.define(version: 20170225234855) do
   create_table "ideas", force: :cascade do |t|
     t.string   "auth0_id"
     t.string   "title"
-    t.text     "description"
+    t.text     "text"
     t.integer  "rank"
     t.boolean  "active"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer  "problem_id"
     t.index ["problem_id"], name: "index_ideas_on_problem_id", using: :btree
   end
