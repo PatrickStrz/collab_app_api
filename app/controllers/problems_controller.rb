55a646d89f5c154e04560bbd447b08e0b67501a7
Patrick Strzelec <PatrickStrz@users.noreply.github.com>
2017-04-09 17:03:50 -0400

    User model (#10)
    * created user model
    
    * user_create_working
    
    * cleaned up comments

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -3,19 +3,15 @@ class ProblemsController < ApplicationController
   # before_action :authenticate, only: [:update, :destroy, :create]
   before_action :set_problem, only: [:show, :update, :destroy]
 
-  # GET /problems
   def index
     problems = Problem.all
 
     render json: problems
   end
-
-  # GET /problems/1
   def show
     render json: @problem
   end
 
-  # POST /problems
   def create
     problem = Problem.new(problem_params)
 
@@ -26,7 +22,6 @@ class ProblemsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /problems/1
   def update
     if @problem.update(problem_params)
       render json: @problem
@@ -35,18 +30,15 @@ class ProblemsController < ApplicationController
     end
   end
 
-  # DELETE /problems/1
   def destroy
     @problem.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_problem
       @problem = Problem.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def problem_params
       params.require(:problem).permit(:auth0_id, :text, :title, :rank, :active)
     end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,65 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+
+  def index
+    @users = User.all
+
+    render json: @users
+  end
+
+  def show
+    render json: @user
+  end
+
+  def create
+    # creates or updates a record, temporary hack
+    #todo: create new action for this to be more clear
+
+    success = create_or_update_user
+
+    if success && success == 'updated'
+      render json: @user, status: :ok, location: @user
+    elsif success && success == 'created'
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy
+  end
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      #todo: make params required
+      params.require(:user).permit(:user_id, :name, :family_name, :given_name, :email, :picture, :picture_large)
+    end
+
+    def create_or_update_user
+      user_id = params[:user_id]
+
+      if User.find_by(user_id: params[:user_id])
+        @user = User.find_by(user_id: params[:user_id])
+        @user.update(user_params) ? 'updated' : false
+      else
+        @user = User.new(user_params)
+        @user.save ? 'created' : false
+      end
+    end
+
+end
