ced0b6b87ba98b72cd6577dfd7e932e1a5fe17f1
PatrickStrz <patrick.strzelec@gmail.com>
2017-04-09 02:19:06 -0400

    user_create_working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,23 +8,26 @@ class UsersController < ApplicationController
     render json: @users
   end
 
-  # GET /users/1
   def show
     render json: @user
   end
 
-  # POST /users
   def create
-    @user = User.new(user_params)
+    # creates or updates a record, temporary hack
+    #todo: create new action for this to be more clear
 
-    if @user.save
+    success = create_or_update_user
+
+    if success && success == 'updated'
+      render json: @user, status: :ok, location: @user
+    elsif success && success == 'created'
       render json: @user, status: :created, location: @user
     else
       render json: @user.errors, status: :unprocessable_entity
     end
+
   end
 
-  # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
       render json: @user
@@ -33,19 +36,31 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
   def destroy
     @user.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_user
       @user = User.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def user_params
+      #todo: make params required
       params.require(:user).permit(:user_id, :name, :family_name, :given_name, :email, :picture, :picture_large)
     end
+
+    def create_or_update_user
+      user_id = params[:user_id]
+
+      if User.find_by(user_id: params[:user_id])
+        @user = User.find_by(user_id: params[:user_id])
+        @user.update(user_params) ? 'updated' : false
+      else
+        @user = User.new(user_params)
+        @user.save ? 'created' : false
+      end
+    end
+
 end
