10a2892ce345f2ccdd7ec6a7d7afdd0e62facc2f
PatrickStrz <patrick.strzelec@gmail.com>
2017-04-12 15:11:31 -0400

    added user association for problems

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   #todo: need to enforce uniquness on db, right now only enforced on applicaiton level
   validates :user_id, uniqueness: true
+  has_many :problems
 end
